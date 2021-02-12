ac1e1b3bee2fe7ad33732b49e1a8544de55498de
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2021-02-12 11:36:02 +0100

    Use test order for keys in json
    - Write new tests on json even if some other test failed

--- munit-snapshot/src/munit/snapshot/SnapshotSuite.scala
+++ munit-snapshot/src/munit/snapshot/SnapshotSuite.scala
@@ -2,6 +2,7 @@ package munit.snapshot
 
 import java.io.BufferedWriter
 import java.io.OutputStreamWriter
+import scala.collection.mutable.LinkedHashMap
 
 import munit._
 import munit.internal.console.StackTraces
@@ -10,8 +11,8 @@ import upickle.default._
 trait SnapshotSuite extends FunSuite {
 
   private var file: os.Path = null
-  private var json: ujson.Obj = null
-  private var initialJson: Map[String, ujson.Value] = null
+  private val json = new LinkedHashMap[String, ujson.Value]
+  private var initialJson: LinkedHashMap[String, ujson.Value] = null
   private var currentTests: Set[String] = null
   private var executedTests = 0
 
@@ -25,28 +26,31 @@ trait SnapshotSuite extends FunSuite {
   )(body: => T)(implicit loc: Location): Unit = test(options) {
     StackTraces.dropInside {
       // First test executed
-      if (file == null && json == null && initialJson == null) {
+      if (executedTests == 0) {
         val path = os.Path(loc.path)
         file = path / os.up / s"${path.last.stripSuffix("scala")}json"
-        json = if(os.isFile(file)) ujson.read(os.read.stream(file)).obj else ujson.Obj()
-        initialJson = json.value.toMap
+        initialJson =
+          if (os.isFile(file)) ujson.read(os.read.stream(file)).obj.value
+          else new LinkedHashMap[String, ujson.Value]
         currentTests = munitTests.view.map(_.name).toSet
       }
       executedTests += 1
-
-      def pair = options.name -> writeJs(body)
-
-      json.value.get(options.name) match {
-        case Some(v) => assertEquals(body, read[T](v))
-        case None => json.value += pair
+      initialJson.get(options.name) match {
+        case Some(v) =>
+          assertEquals(body, read[T](v))
+          json += options.name -> v
+        case None =>
+          json += options.name -> writeJs(body)
       }
     }
     // Last test executed
-    if(executedTests == currentTests.size) {
-      val withoutStales = json.value.iterator.filter { case (k, _) => currentTests(k) }.toSeq.sortBy(_._1).toMap
-      if (withoutStales != initialJson) {
-        val writer = new BufferedWriter(new OutputStreamWriter(os.write.over.outputStream(file)))
-        ujson.writeTo(withoutStales, writer, 2)
+    if (executedTests == currentTests.size) {
+      val withoutStales = json.filter { case (k, _) => currentTests(k) }
+      if (withoutStales.nonEmpty) {
+        val writer = new BufferedWriter(
+          new OutputStreamWriter(os.write.over.outputStream(file))
+        )
+        ujson.writeTo(initialJson ++ withoutStales, writer, 2)
         writer.write('\n')
         writer.close()
       }
